f502ba0d1ab6cc8678437747321ce0615ab09811
aabilio <aabilio@gmail.com>
2011-04-17 17:05:08 +0000

    Añadiendo setups

---
+++ setup_windows.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of PyDownTV.
+#
+#    PyDownTV is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PyDownTV is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PyDownTV.  If not, see <http://www.gnu.org/licenses/>.
+
+# Archivo setup.py del proyecto PyDownTV:
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+#from distutils.core import setup
+from setuptools import setup
+
+import py2exe
+
+setup(name="PydownTV", 
+    version="1.0-Beta",
+    description="Descarga vídeos de las webs de TVs Españolas",
+    author="Abilio Almeida Eiroa",
+    author_email="aabilio@gmail.com",
+    url="http://code.google.com/p/pydowntv/",
+    license="GPL3",
+    scripts=["pydowntv.py"], 
+    packages=["Servers", "Servers.pyaxel"], 
+    py_modules=["tve", "rtve", "a3", "telecinco", "Descargar", "pyaxel"], 
+    console=["pydowntv.py"], 
+    options={"py2exe": {"bundle_files": 1}},
+    zipfile=None
+)
+    
+    
